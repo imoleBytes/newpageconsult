62b99ed35103ed7e65a60c217388fc6da6966b60
imole <dremkay71@gmail.com>
2023-01-08 04:16:38 +0100

    index js created and some mobile menu functionality added

---
+++ index.js
@@ -0,0 +1,17 @@
+const bar = document.querySelector('.bar');
+const m_menu = document.querySelector('.m-menu');
+
+
+bar.addEventListener('click',toggle);
+
+function toggle(){
+    if (m_menu.classList.contains('t')){
+        m_menu.classList.remove('t');
+        setTimeout(()=> {
+            m_menu.classList.add('t');      
+        },4000)
+    }else{
+        m_menu.classList.add('t');
+    }
+    
+}
